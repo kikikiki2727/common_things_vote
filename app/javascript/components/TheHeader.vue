1a9ecce1abbe95ccb6878b1e1cbe79bff0b3ab5a
kikikiki2727 <kimori27271212@gmail.com>
2021-08-28 01:38:08 +0900

    Add: ヘッダーとフッターを追加

---
+++ app/javascript/components/TheHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+      <a class="navbar-brand" href="#">あるあるネタ投票</a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">投票</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ユーザー登録</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ログイン</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+}
+</script>
+
+<style scoped>
+  .navbar {
+    background-color: rgba(192,192,192,0.3);
+  }
+  .navbar-nav {
+    flex-direction: row;
+  }
+  .nav-item {
+    padding-left: 5px;
+  }
+</style>

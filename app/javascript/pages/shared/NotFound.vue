60cda1dc2c1304bddfc9707075aaefa0f17b563b
kikikiki2727 <kimori27271212@gmail.com>
2021-09-08 09:26:19 +0900

    Add: notfoundエラーページを追加

---
+++ app/javascript/pages/shared/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="not_found">
+    <div class="error_contents">
+      <p>お探しのページが見つかりませんでした。</p>
+      <p>一時的にアクセスができない状況にあるか、</p>
+      <p>移動もしくは削除された可能性があります。</p>
+      <p>URlにお間違えがないか再度ご確認ください。</p>
+    </div>
+    <div class="error_btn">
+      <router-link
+        :to="{ name: 'JokeIndex' }"
+        class="return_btn"
+      >
+        <v-btn>
+          トップページに戻る
+        </v-btn>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
+
+<style scoped>
+  .not_found {
+    text-align: center;
+  }
+  .return_btn {
+    text-decoration: none;
+  }
+</style>
